53eba3970b27ff307105de6076f87fc7d307902f
iru <jinsei100nen.douikiruka@gmail.com>
2023-03-25 08:20:33 +0900

    新規作成

---
+++ japanbaseball/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  japanbaseball
+//
+//  Created by 松尾健司 on 2023/03/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ japanbaseball/japanbaseballApp.swift
@@ -0,0 +1,17 @@
+//
+//  japanbaseballApp.swift
+//  japanbaseball
+//
+//  Created by 松尾健司 on 2023/03/25.
+//
+
+import SwiftUI
+
+@main
+struct japanbaseballApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
